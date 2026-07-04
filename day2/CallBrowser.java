87b9115550210ac077748f48d2dacd0ce7b2f521
ilakkiyamani8198 <ilakkiyamani8198@gmail.com>
2026-07-04 07:54:32 +0530

    Add files via upload

---
+++ day2/CallBrowser.java
@@ -0,0 +1,9 @@
+package org.testleaf.week1.day2;
+
+public class CallBrowser {
+    public static void main(String[] args) {
+        Browser br=new Browser();
+    br.launchBrowser("Call Browser");
+    br.loadUrl();
+    }
+    }
